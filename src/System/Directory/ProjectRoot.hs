15d6328b7047ddc662329463a018cfeff3b2951c
yamadapc <tacla.yamada@gmail.com>
2015-12-04 08:52:03 -0200

    Initial commit

---
+++ src/System/Directory/ProjectRoot.hs
@@ -0,0 +1,37 @@
+-- |
+-- Simple way of finding the root of a project given an entry-point.
+-- This module provides bindings to the @projectroot@ C library
+module System.Directory.ProjectRoot
+  where
+
+import           Control.Monad    ((<=<), (>=>))
+import           Foreign.C
+import           System.Directory (getCurrentDirectory)
+
+-- * Haskell Wrappers
+
+-- |
+-- Find the project root given an entry point
+getProjectRoot :: FilePath -> IO FilePath
+getProjectRoot = (find_project_root <=< newCString) >=> peekCString
+
+-- |
+-- Find the weighted project root given an entry point
+getProjectRootWeighted :: FilePath -> IO FilePath
+getProjectRootWeighted = (find_project_root_weighted <=< newCString) >=>
+                         peekCString
+
+-- |
+-- Find the project root of the current directory
+getProjectRootCurrent :: IO FilePath
+getProjectRootCurrent = getCurrentDirectory >>= getProjectRoot
+
+-- |
+-- Find the weighted project root of the current directory
+getProjectRootWeightedCurrent :: IO FilePath
+getProjectRootWeightedCurrent = getCurrentDirectory >>= getProjectRootWeighted
+
+-- * C functions
+
+foreign import ccall "find_project_root" find_project_root :: CString -> IO CString
+foreign import ccall "find_project_root_weighted" find_project_root_weighted :: CString -> IO CString
